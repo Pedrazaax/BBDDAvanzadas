df82835d15b3bc97e91698c9d20048146a304022
Pedrazaax <carlos.pedraza@alu.uclm.es>
2023-02-23 18:59:39 +0100

    Cambio de ruta

--- datos.py
+++ datos.py
@@ -5,7 +5,7 @@ conn = sqlite3.connect('salarios.db')
 datos = []
 
 def cargarDatos():
-    csvs =['BBDDAvanzadas/microdatos/EES_2002.csv','BBDDAvanzadas/microdatos/EES_2006.csv','BBDDAvanzadas/microdatos/EES_2010.csv','BBDDAvanzadas/microdatos/EES_2014.csv','BBDDAvanzadas/microdatos/EES_2018.csv']  #Para cargar todos los csvs a la vez
+    csvs =['./microdatos/EES_2002.csv','./microdatos/EES_2006.csv','./microdatos/EES_2010.csv','./microdatos/EES_2014.csv','./microdatos/EES_2018.csv']  #Para cargar todos los csvs a la vez
     contCuatrienal= 1 #Sirve para saber en que cuatrianual estamos sabiendo que empezamos en 1995
     # la primera fila la toma para el nombre de los campos y a partir de ahi añade los datos
     for archivo in csvs:   #archivo representa al csv que se va a cargar
@@ -24,8 +24,6 @@ def crearTabla():
 
 
 def insercionDatos():
-    #query = "INSERT INTO datosMercadoLaboral (ORDENCCC, ORDENTRA, NUTS1, CNACE, ESTRATO2, CONTROL, MERCADO, SEXO, TIPOPAIS, CNO1, RESPONSA, ESTU, MESANTI, ANOANTI, TIPOJOR, VAL, VAN, PUENTES, JAP, JSP1, JSP2, HEXTRA, SALBASE, EXTRAORM, PHEXTRA, COMSAL, COMSALTT, IRPFMES, COTIZA, DIAS, SALBRUTO, PEXTRAAF, PEXTRAAV, VESP, AFECANO, MESCOMA, DIACOMA, ANOS2, FACTOTAL, CUATRIENAL) VALUES (:ORDENCCC, :ORDENTRA, :NUTS1, :CNACE, :ESTRATO2, :CONTROL, :MERCADO, :SEXO, :TIPOPAIS, :CNO1, :RESPONSA, :ESTU, :MESANTI, :ANOANTI, :TIPOJOR, :VAL, :VAN, :PUENTES, :JAP, :JSP1,:JSP2, :HEXTRA, :SALBASE, :EXTRAORM, :PHEXTRA, :COMSAL, :COMSALTT, :IRPFMES, :COTIZA, :DIAS, :SALBRUTO, :PEXTRAAF, :PEXTRAAV, :VESP, :AFECANO, :MESCOMA, :DIACOMA, :ANOS2, :FACTOTAL, :CUATRIENAL)"
-    #query = "INSERT INTO datosMercadoLaboral (ORDENCCC, ORDENTRA, NUTS1,CNACE,ESTRATO2,CONTROL,MERCADO,CONVENIO,SEXO,TIPOPAIS,CNO1,RESPONSA,ESTU,MESANTI,ANOANTI,TIPOJOR,TC,VAL,VAN,PUENTES,JAP,JSP,HEXTRA,SALBASE,EXTRAORM,PHEXTRA,COMSAL,COMSALTT,COMSALV,IRPFMES,COTIZA,AFECMES,DIAS,SALBRUTO,PEXTRAAF,PEXTRAAV,VESP,AFECANO,MESCOMA,DIACOMA,ANOS2,FACTOTAL,CUATRIENAL) VALUES (?, ?, ?, ?, ? ,?, ?, ?, ?, ?,?, ?, ?, ?, ? ,?, ?, ?, ?, ?,?, ?, ?, ?, ? ,?, ?, ?, ?, ?,?, ?, ?, ?, ? ,?, ?, ?, ?, ?,?,?,?)"
     query = "INSERT INTO datosMercadoLaboral (ORDENCCC ,CONTROL, RESPONSA, NUTS1, EXTRAORM, SALBASE, IRPFMES, ANOANTI, MERCADO, TIPOJOR, FACTOTAL, VAL, CNO1, MESANTI, COMSALTT, COTIZA, VAN, ESTU, ESTRATO2, PHEXTRA, COMSAL, ORDENTRA, SEXO, PUENTES, HEXTRA, ANOS2, JAP, CNACE, TIPOPAIS, CUATRIENAL) VALUES (:ORDENCCC, :CONTROL, :RESPONSA, :NUTS1, :EXTRAORM, :SALBASE, :IRPFMES, :ANOANTI, :MERCADO, :TIPOJOR, :FACTOTAL, :VAL, :CNO1, :MESANTI, :COMSALTT, :COTIZA, :VAN, :ESTU, :ESTRATO2, :PHEXTRA, :COMSAL, :ORDENTRA, :SEXO, :PUENTES, :HEXTRA, :ANOS2, :JAP, :CNACE, :TIPOPAIS, :CUATRIENAL)"
     conn.executemany(query, datos)
     print("Datos insertados con exito")
